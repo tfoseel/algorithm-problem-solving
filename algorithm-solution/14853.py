d92325da6aa2df2440ce004abfcfa60d1b974d42
tfoseel <tfoseel@gmail.com>
2022-10-05 03:08:39 +0900

    2022.10.05. Solve problem

---
+++ algorithm-solution/14853.py
@@ -0,0 +1,40 @@
+# BOJ 14853. 동전 던지기
+
+import sys
+
+
+def solve():
+    # Input
+    v = sys.stdin.readline()
+    v = list(map(int, v.split()))
+    n1, m1, n2, m2 = v[0], v[1], v[2], v[3]
+    # p and q follow uniform distribution, so it can be thought as picking one real number from [0, 1].
+    # A toss of coin P (or Q) can be thought as picking one real number x from [0, 1],
+    # considering the result 'head' if x > p (or q) and 'tail' otherwise.
+    # Therefore, the problem can be reduced into picking (n1 + 1) points about coin P
+    # among (n1 + n2 + 2) points, which are on number line [0, 1].
+    # When (n1 + 1) points are chosen, left m1 points indicate head of coin P toss,
+    # next one point indicates value of p, and last (n1 - m1) points indicate tail of coin P toss.
+    # Remaining (n2 + 1) points can be labeled with the same rule about coin Q.
+    # Consider point x = p on the line.
+    # There can be m1 points at minimum and (m1 + m2) points at maximum, on the left.
+    # If there are k(m1 <= k <= m1 + m2) points on the left,
+    # we can choose m1 points among them and (n1 - m1) points among (n1 + n2 + 1 - k) points on the right about coin P.
+    # Then, we can determine q uniquely, by finding (m2 + 1)-th point among remaining points from the left.
+    # Here, p < q always holds.
+    # To compute these values takes a lot of time, so first try to compute when k = m1 and reuse the value for other k.
+    base = 1
+    for i in range (0, n2 + 1):
+        base = base * (n1 - m1 + 1 + i) / (n1 + 2 + i)
+    answer = base
+    # Here, when k = m1 + i, the desired value is base * (m1 + i) * (n2 + 2 - i) / i / (n1 + n2 - m1 + 2 - i).
+    for i in range(1, m2 + 1):
+        base *= (m1 + i) * (n2 + 2 - i) / i / (n1 + n2 - m1 + 2 - i)
+        answer += base
+    # Output
+    sys.stdout.write(str(answer) + '\n')
+
+
+n = int(input())
+for i in range(n):
+    solve()
---
+++ algorithm-solution/2098.cpp
@@ -0,0 +1,83 @@
+// BOJ 2098. 외판원 순회
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+const int INF {99999999};
+int n;
+
+// costs[i][j] is cost to navigate from vertice i to j.
+std::vector<std::vector<int>> costs;
+
+// Let C(S, i) be a minimum cost when visiting set of vertices S and terminates at vertice i.
+// dp[x] is minimum cost where x.first == (an integer encoding for S) and x.second == i.
+std::map<std::pair<int, int>, int> dp;
+
+// Encode string representation of set S to integer.
+// For example, when S == {1, 0, 0, 1}, and s == "1001", it returns 9(==1001(2)).
+int encode_set(std::string s) {
+    int x {0};
+    for (int i=0; i<s.size(); i++) x += (1 << (s.size() - 1 - i)) * (s[i] == '1');
+    return x;
+}
+
+// solve(size) is a process of getting C(S, j) for each vertice j != 0 in S,
+// where |S| = size using dynamic programming.
+void solve(int size) {
+    if (size == 2) {
+        // A path consists of one edge.
+        for (int i=0; i<n; i++)
+            dp[std::make_pair((1 << (n - 1)) + (1 << (n - i - 1)), i)] = costs[0][i];
+        return;
+    }
+    std::string s = std::string(n - size, '0') + std::string(size - 1, '1');
+    do {
+        // "1" + s is subset of given size, where subset[0] == 1.
+        std::string subset = "1" + s;
+        // Disallow to return to vertice 0.
+        dp[std::make_pair(encode_set(subset), 0)] = INF;
+        // For any vertice j != 0 in subset S, get C(S, j) from partial answers C(S - {j}, i).
+        for (int j=1; j<subset.size(); j++) {
+            if (subset[j] == '0') continue;
+            // C(S, j) == min(C(S - {j}, i)) where vertice i != 0 is in set S - {j}.
+            subset[j] = '0';
+            int sub_answer {INF};
+            for (int i=1; i<subset.size(); i++) {
+                if (subset[i] == '1')  {
+                    sub_answer = std::min(
+                        sub_answer, dp[std::make_pair(encode_set(subset), i)] + costs[i][j]
+                    );
+                }
+            }
+            subset[j] = '1';
+            // Store answer of subproblems into map for reuse.
+            dp[std::make_pair(encode_set(subset), j)] = sub_answer;
+        }
+    } while (std::next_permutation(s.begin(), s.end()));
+}
+
+int main() {
+    // Input
+    std::cin >> n;
+    for (int i=0; i<n; i++) {
+        costs.push_back(std::vector<int>());
+        for (int j=0; j<n; j++) {
+            int x;
+            std::cin >> x;
+            if (x == 0) x = INF;
+            costs[i].push_back(x);
+        }
+    }
+    // Solve
+    dp[std::make_pair(0, 0)] = 0;
+    for (int size=2; size<=n; size++) solve(size);
+    int answer {INF};
+    for (int i=1; i<n; i++)
+        answer = std::min(answer, dp[std::make_pair((1 << n) - 1, i)] + costs[i][0]);
+    // Output
+    std::cout << answer << std::endl;
+    return 0;
+}
